0589232b7016d0dd9952f537de2d51fe21f4e5e6
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-03-24 22:00:41 +0600

    create healthcare model

--- formalyze/form/models.py
+++ formalyze/form/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
+from django.conf import settings
 # Create your models here.
 
 role_choices= [
     ('admin', 'Admin'),
     ('patient', 'Patient')
+]
+
+form_type=[
+    ('appointment', 'Appointment'),
+    ('referral', 'Referral'),
+    ('employment', 'Employment')
 ]
 
 class User(AbstractUser):
@@ -17,4 +25,14 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
     
+    
+class HealthCare(models.Model):
+    name=models.CharField(max_length=100)
+    address=models.CharField(max_length=200)
+    phone=models.CharField(max_length=15)
+    email=models.EmailField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
     
