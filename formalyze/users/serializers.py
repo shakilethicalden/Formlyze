27c5d6ed2be9b59caef00ecbc7479672f9bda4d3
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-04-03 13:12:43 +0600

    update all

--- formalyze/users/serializers.py
+++ formalyze/users/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProfile
-        fields = ['username', 'email', 'first_name', 'last_name', 'healthCareName', 'address', 'phone']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'healthCareName', 'address', 'phone']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
