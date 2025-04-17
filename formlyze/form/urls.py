152e12128c2926ef555f9edd869fc87ce203412b
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-04-17 23:54:56 +0600

    update url.py for features

--- formlyze/form/urls.py
+++ formlyze/form/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import FormView, FormResponseView, form_details
+from .views import FormView, FormResponseView, form_details, ToggleFavoriteView, ToggleArchiveView, ToggleTrashView
 
 
 router = DefaultRouter()
@@ -11,6 +11,10 @@ router.register('response',FormResponseView, basename='form-response')
 urlpatterns = [
     path('', include(router.urls)),
     path('<uuid:unique_token>/', form_details, name='form_details'),
+    path('toggle-favorite/<int:pk>', ToggleFavoriteView.as_view(), name='toggle_favorite'),
+     path('toggle-archive/<int:pk>', ToggleArchiveView.as_view(), name='toggle_archive'),
+     path('toggle-trash/<int:pk>', ToggleTrashView.as_view(), name='toggle_trash'),
+    
 
     
 ]
