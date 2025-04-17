4203ef919a2694cbb39c9b38f424f6d0bc318cdf
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-04-17 19:09:06 +0600

    remove unecessary views

--- formlyze/form/urls.py
+++ formlyze/form/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import FormView, FormResponseView, form_details, ExportFormResponsesExcel, ExcelDownloadDetails
+from .views import FormView, FormResponseView, form_details
 
 
 router = DefaultRouter()
@@ -11,8 +11,6 @@ router.register('response',FormResponseView, basename='form-response')
 urlpatterns = [
     path('', include(router.urls)),
     path('<uuid:unique_token>/', form_details, name='form_details'),
-    path("export-responses/<int:form_id>/", ExportFormResponsesExcel.as_view(), name="export-responses"),
-    path("download-excel/<int:form_id>/", ExcelDownloadDetails.as_view(), name="download-excel"),
-    
+
     
 ]
