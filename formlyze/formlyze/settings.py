97af6a5e0b293b74ae6766364f7d6d09823d2fac
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-04-23 19:37:09 +0600

    mysql database setup, update requiremnets, fix errors

--- formlyze/formlyze/settings.py
+++ formlyze/formlyze/settings.py
@@ -148,12 +148,24 @@ WSGI_APPLICATION = 'formlyze.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
+
 
 
 # Password validation
