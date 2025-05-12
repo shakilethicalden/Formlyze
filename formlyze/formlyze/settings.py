7c6e7b3e0d6e9f37bc8bdce1513d323fe84b6c67
Md. Shakil <shakil.eub.cse@gmail.com>
2025-05-12 18:15:23 +0600

    Change the email and google auth configuration

--- formlyze/formlyze/settings.py
+++ formlyze/formlyze/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env.production", override=True)
+load_dotenv(BASE_DIR / ".env", override=True)
 
 
 # Quick-start development settings - unsuitable for production
